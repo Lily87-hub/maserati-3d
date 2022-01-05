fafe76661afcefafd9929fd6505e0d6c32a762fd
Elizabeth Afolabi <elizabeth.afolabi@joza-it.fr>
2022-01-05 17:17:11 +0100

    Dropup update

--- src/components/elements-menu/menu.js
+++ src/components/elements-menu/menu.js
@@ -7,33 +7,55 @@ const Menu = () => {
             <a href="#">
                 <img src="./media/icon_color/icon_color_normal.png" alt="color"/>
             </a>
-            <div className="overlay">
-                <a href="#" className="icon">
-                    <img src="./media/icon_color/icon_color_hover.png" alt="hover"/>
-                </a>
+            <div className="dropup-colour">
+                <div className="overlay-colour">
+                    <a href="#" className="icon-colour">
+                        <img src="./media/icon_color/icon_color_hover.png" alt="couleur"/>
+                    </a>
+                </div>
+                <div className="dropup-content">
+                    <a>
+                        <img src="./media/Vignettes/carpaint_BiancoAlpi.png" alt=""/>
+                    </a>
+                    <a>
+                        <img src="./media/Vignettes/carpaint_Grigio.png" alt=""/>
+                    </a>
+                    <a>
+                        <img src="./media/Vignettes/carpaint_GrigioMaratea.png" alt=""/>
+                    </a>
+                    <a>
+                        <img src="./media/Vignettes/carpaint_NeroRibelle.png" alt=""/>
+                    </a>
+                    <a>
+                        <img src="./media/Vignettes/carpaint_RossoFolgore.png" alt=""/>
+                    </a>
+                </div>
             </div>
+
+
+
             <a href="#">
                 <img src="./media/icon_wheel/icon_wheel_normal.png" alt="wheel"/>
             </a>
-            <div className="overlay">
-                <a href="#" className="icon">
-                    <img src="./media/icon_wheel/icon_wheel_hover.png" alt="wheel"/>
+            <div className="overlay-wheel">
+                <a href="#" className="icon-wheel">
+                    <img src="./media/icon_wheel/icon_wheel_hover.png" alt="jantes"/>
                 </a>
             </div>
             <a href="#">
                 <img src="./media/icon_door/icon_door_normal.png" alt="door"/>
             </a>
-            <div className="overlay">
-                <a href="#" className="icon">
-                    <img src="./media/icon_door/icon_door_hover.png" alt="door"/>
+            <div className="overlay-door">
+                <a href="#" className="icon-door">
+                    <img src="./media/icon_door/icon_door_hover.png" alt="porte"/>
                 </a>
             </div>
             <a href="#">
                 <img src="./media/icon_view/icon_view_normal.png" alt="view"/>
             </a>
-            <div className="overlay">
-                <a href="#" className="icon">
-                    <img src="./media/icon_view/icon_view_hover.png" alt="view"/>
+            <div className="overlay-view">
+                <a href="#" className="icon-view">
+                    <img src="./media/icon_view/icon_view_hover.png" alt="vue"/>
                 </a>
             </div>
 
